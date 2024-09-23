5e6e41300de152b6f1bdf548c51ffa08cfde81a9
s-duu-jp <s@duu.jp>
2024-09-23 16:08:07 +0900

    postCreateCommand.sh変更

--- .devcontainer/sh/postCreateCommand.sh
+++ .devcontainer/sh/postCreateCommand.sh
@@ -1,1 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# 作業ディレクトリ
+dir="/workspace"
+
+# .env.dev ファイルが存在しない場合は作成して初期値を設定
+if [ ! -f ${dir}/.env.dev ]; then
+  echo 'ENV="development"' > ${dir}/.env.dev
+fi
+
+# .env.stg ファイルが存在しない場合は作成して初期値を設定
+if [ ! -f ${dir}/.env.stg ]; then
+  echo 'ENV="staging"' > ${dir}/.env.stg
+fi
+
+# .env.prod ファイルが存在しない場合は作成して初期値を設定
+if [ ! -f ${dir}/.env.prod ]; then
+  echo 'ENV="production"' > ${dir}/.env.prod
+fi
