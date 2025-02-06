73115388086fe966e4b4cb28faefc4919fd1c10a
s-duu-jp <s@duu.jp>
2025-02-07 08:14:19 +0900

    shell updated

--- .devcontainer/sh/postCreateCommand.sh
+++ .devcontainer/sh/postCreateCommand.sh
@@ -3,17 +3,8 @@
 # 作業ディレクトリ
 dir="/workspace"
 
-# .env.dev ファイルが存在しない場合は作成して初期値を設定
-if [ ! -f ${dir}/.env.dev ]; then
-  echo 'ENV="development"' > ${dir}/.env.dev
-fi
-
-# .env.stg ファイルが存在しない場合は作成して初期値を設定
-if [ ! -f ${dir}/.env.stg ]; then
-  echo 'ENV="staging"' > ${dir}/.env.stg
-fi
-
-# .env.prod ファイルが存在しない場合は作成して初期値を設定
-if [ ! -f ${dir}/.env.prod ]; then
-  echo 'ENV="production"' > ${dir}/.env.prod
-fi
+# envファイル初期設定
+declare -A envs=( ["dev"]="development" ["stg"]="staging" ["prod"]="production" )
+for env in "${!envs[@]}"; do
+  [ -f "$dir/.env.$env" ] || echo "ENV=\"${envs[$env]}\"" > "$dir/.env.$env"
+done
