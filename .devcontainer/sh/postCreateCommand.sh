4924f74541bc07d63b0d571dae0e910c76199a85
s-duu-jp <s@duu.jp>
2025-02-09 08:57:22 +0900

    cpするようにｓた「

--- .devcontainer/sh/postCreateCommand.sh
+++ .devcontainer/sh/postCreateCommand.sh
@@ -6,5 +6,5 @@ dir="/workspace"
 # envファイル初期設定
 declare -A envs=( ["dev"]="development" ["stg"]="staging" ["prod"]="production" )
 for env in "${!envs[@]}"; do
-  [ -f "$dir/.env.$env" ] || echo "ENV=\"${envs[$env]}\"" > "$dir/.env.$env"
+  [ -f "$dir/.env.$env" ] || cp -rp "$dir/.env_example" "$dir/.env.$env"
 done
